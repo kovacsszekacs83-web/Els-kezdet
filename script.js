fc04f077d70c14eeb074c3dfe149bcef632618d1
kovacsszekacs83-web <kovacsszekacs83@gmail.com>
2025-08-20 21:56:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Egyszerű JavaScript teszt
-console.log("Weboldal betöltve ✅");
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav ul li a");
+  links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+      link.style.color = "#ff6600";
+    });
+    link.addEventListener("mouseout", () => {
+      link.style.color = "#fff";
+    });
+  });
+});
